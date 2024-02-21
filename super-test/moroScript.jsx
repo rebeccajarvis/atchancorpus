7e7c5e335767d3cb6944bc334cf6e63f9f883ea7
Marie-Anne Xu <marieanne.xu@berkeley.edu>
2024-02-21 11:27:39 -0800

    embedded youtube video

--- super-test/moroScript.jsx
+++ super-test/moroScript.jsx
@@ -894,8 +894,11 @@
                     </div>
                   </div>
 
-                  <iframe width="420" height="315"
-                    src="https://youtu.be/fMIiQwCIzGQ">
+                  <iframe width="560" height="315" 
+                  src="https://www.youtube.com/embed/fMIiQwCIzGQ?si=d3gMisqGeOdMJyw1" 
+                  title="YouTube video player" 
+                  frameborder="0" 
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
                   </iframe>
                 
                 </div>

a0b0a89d0a790f0abf01eb1e8305f9eb6d022593
Marie-Anne Xu <marieanne.xu@berkeley.edu>
2024-04-05 12:47:01 -0700

    added en orthography

--- sep-link/pages/Orthography.js
+++ sep-link/pages/Orthography.js
@@ -1,9 +1,217 @@
 var Orthography = React.createClass(
         
     {render: function() {
-        return   <div> </div>
+        return   <div className='ui text container'>
+            <h1 className='ui dividing header'>Orthography</h1>
+            <table className='ui celled table '>
+            <thead>
+              <tr><th>IPA</th>
+              <th>Orthography</th>
+              <th>Example</th>
+            </tr></thead>
+  
+            <tbody>
+            <tr>
+              <td data-label="IPA">p</td>
+              <td data-label="Orthography">p</td>
+              <td data-label="Job">ápɛn ‘debt’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">pʰ</td>
+              <td data-label="Orthography"><b>ph</b></td>
+              <td data-label="Example">áphɛ̂n ‘twenty’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">b</td>
+              <td data-label="Orthography">b</td>
+              <td data-label="Example">ábɛ ‘paddle’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">t</td>
+                <td data-label="Orthography">t</td>
+                <td data-label="Example">áta ‘insult’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">tʰ</td>
+              <td data-label="Orthography"><b>th</b></td>
+              <td data-label="Example">átha ‘war’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">d</td>
+                <td data-label="Orthography">d</td>
+                <td data-label="Example">du ‘snake’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">c</td>
+                <td data-label="Orthography">c</td>
+                <td data-label="Example">ácɔn ‘fish’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">cʰ</td>
+              <td data-label="Orthography"><b>ch</b></td>
+              <td data-label="Example">chɔn ‘hen’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">ɟ</td>
+              <td data-label="Orthography"><b>j</b></td>
+              <td data-label="Example">ájí ‘respect’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">k</td>
+                <td data-label="Orthography">k</td>
+                <td data-label="Example">áko ‘hunger’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">kʰ</td>
+              <td data-label="Orthography"><b>kh</b></td>
+              <td data-label="Example">ákhɔ́ ‘friendship’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">g</td>
+                <td data-label="Orthography">g</td>
+                <td data-label="Example">gwe ‘sea’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">kp</td>
+                <td data-label="Orthography">kp</td>
+                <td data-label="Example">kpakpâ ‘papaya’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">gb</td>
+                <td data-label="Orthography">gb</td>
+                <td data-label="Example">gba ‘dog’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">f</td>
+                <td data-label="Orthography">f</td>
+                <td data-label="Example">ńfɛ ‘eggplant’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">v</td>
+                <td data-label="Orthography">v</td>
+                <td data-label="Example">nvrâ ‘appattam’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">s</td>
+                <td data-label="Orthography">s</td>
+                <td data-label="Example">asa ‘gecko’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">z</td>
+                <td data-label="Orthography">z</td>
+                <td data-label="Example">zúmwɛn ‘fish species’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">h</td>
+                <td data-label="Orthography">h</td>
+                <td data-label="Example">áhɔn ‘ax’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">ɓ</td>
+              <td data-label="Orthography"><b>bh</b></td>
+              <td data-label="Example">ábhɛ́ ‘rope’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">l</td>
+                <td data-label="Orthography">l</td>
+                <td data-label="Example">la ‘to pray’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">j</td>
+              <td data-label="Orthography"><b>y</b></td>
+              <td data-label="Example">áyá ‘tree’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">w</td>
+                <td data-label="Orthography">w</td>
+                <td data-label="Example">awɔ́ ‘cat’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">m</td>
+                <td data-label="Orthography">m</td>
+                <td data-label="Example">ńmɔ́n ‘arm’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">n</td>
+                <td data-label="Orthography">n</td>
+                <td data-label="Example">ánɛn ‘hoe’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">ɗ</td>
+              <td data-label="Orthography"><b>l</b></td>
+              <td data-label="Example">lyali ‘to forget’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">r</td>
+                <td data-label="Orthography">r</td>
+                <td data-label="Example">bwri ‘rat’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">ɲ</td>
+              <td data-label="Orthography"><b>y</b></td>
+              <td data-label="Example">yɛn ‘to know’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">ŋʷ</td>
+              <td data-label="Orthography"><b>w</b></td>
+              <td data-label="Example">wan ‘to crawl’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">i</td>
+                <td data-label="Orthography">i</td>
+                <td data-label="Example">áyí ‘thing’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">e</td>
+                <td data-label="Orthography">e</td>
+                <td data-label="Example">áde ‘antelope’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">ɛ</td>
+                <td data-label="Orthography">ɛ</td>
+                <td data-label="Example">ádɛ ‘palm tree’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">ɛ̃</td>
+              <td data-label="Orthography"><b>ɛn</b></td>
+              <td data-label="Example">átɛn ‘fire’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">a</td>
+                <td data-label="Orthography">a</td>
+                <td data-label="Example">áyá ‘tree’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">ã</td>
+              <td data-label="Orthography"><b>an</b></td>
+              <td data-label="Example">bran ‘animal’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">ɔ</td>
+                <td data-label="Orthography">ɔ</td>
+                <td data-label="Example">ákɔ ‘attic’</td>
+            </tr>
+            <tr>
+              <td data-label="IPA">ɔ̃</td>
+              <td data-label="Orthography"><b>ɔn</b></td>
+              <td data-label="Example">ńmɔ́n ‘arm’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">o</td>
+                <td data-label="Orthography">o</td>
+                <td data-label="Example">agbo ‘door’</td>
+            </tr>
+            <tr>
+                <td data-label="IPA">u</td>
+                <td data-label="Orthography">u</td>
+                <td data-label="Example">du ‘snake’</td>
+            </tr>
+  
+          </tbody>
+  
+          </table>
+        
+        </div>
     }
-}
-)
-
-export default Orthography;
+  }
+  )

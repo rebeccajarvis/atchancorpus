d48df722ee10ffd1d4d6a1d108e5486e37d7d8c6
Marie-Anne Xu <marieanne.xu@berkeley.edu>
2024-04-30 10:43:11 -0700

    fixed image formatting

--- sep-link/atchanScriptFHB.jsx
+++ sep-link/atchanScriptFHB.jsx
@@ -82,12 +82,15 @@ var Homepage = React.createClass(
   return   <div className='ui text container'> 
 
 
-      <h1 className='ui dividing header'>Atchan Song and Story Corpus</h1>
+    <h1 className='ui dividing header'>Atchan Song and Story Corpus</h1>
+    <h2 >ÁCAN NANMƐ LÊ ÁLƐ́BHƆ́ </h2>
 
-      <img className="ui medium center floated rounded image" src="./images/Anono_street.jpg"></img>
-      <img className="ui medium center floated rounded image" src="./images/Blockhauss_ceremony.jpg"></img>
-  
-  <p>This website contains a collection of texts, songs, and stories in the Atchan language (also known as Ébrié). 
+    <div className="ui segment">
+    <img className="ui medium spaced rounded image" src="./images/Anono_street.jpg"></img>
+    <img className="ui medium spaced rounded image" src="./images/Blockhauss_ceremony.jpg"></img>
+    </div>
+        
+    <p>This website contains a collection of texts, songs, and stories in the Atchan language (also known as Ébrié). 
       Atchan is a language spoken by the Tchaman people (also called the Ébrié people), who live in approximately sixty villages located within and near the city of Abidjan, Côte d’Ivoire’s economic capital. 
       Following the classification of Williamson & Blench (2000:18), Atchan, along with its sister language Nghlwa (Mbatto), forms the Potou subgroup in the Kwa (and broader Niger-Congo) language family. 
       Through the resources on this page, you can learn more about the Tchaman people, their language, and their culture.</p> 
@@ -96,27 +99,27 @@ var Homepage = React.createClass(
     
     <p>This website is intended first and foremost as a resource for the Tchaman people, and in addition for researchers who are interested in learning more about the Atchan language.</p>
     
-  <h1 className='ui dividing header'>Project members</h1>
+    <h1 className='ui dividing header'>Project members</h1>
 
-  {/* <h2> (Visit the Storytellers page to learn more about the people who you see and hear in the texts.) </h2> */}
+    {/* <h2> (Visit the Storytellers page to learn more about the people who you see and hear in the texts.) </h2> */}
 
-  <h3> Yao Maxime Dido </h3>
-      <p> Prof. Dido is a native speaker of Atchan, originally from the village of Blockhauss. He published multiple articles about his mother tongue between 2018 and 2021, and he plans to publish more in the coming months. </p> 
+    <h3> Yao Maxime Dido </h3>
+        <p> Prof. Dido is a native speaker of Atchan, originally from the village of Blockhauss. He published multiple articles about his mother tongue between 2018 and 2021, and he plans to publish more in the coming months. </p> 
 
-  <h3> Becky Jarvis </h3>
-      <p> <a href="https://sites.google.com/berkeley.edu/rebecca-jarvis/">Becky</a> is a Ph.D. candidate in linguistics at UC Berkeley. </p>
+    <h3> Becky Jarvis </h3>
+        <p> <a href="https://sites.google.com/berkeley.edu/rebecca-jarvis/">Becky</a> is a Ph.D. candidate in linguistics at UC Berkeley. </p>
 
-  <h3> Katie Russell </h3>
-      <p> <a href="https://www.krrussell.com">Katie</a> is a Ph.D. candidate in linguistics at UC Berkeley. </p> 
+    <h3> Katie Russell </h3>
+        <p> <a href="https://www.krrussell.com">Katie</a> is a Ph.D. candidate in linguistics at UC Berkeley. </p> 
 
-  <h3> Siddharth Ganapathy </h3>
-      <p> Siddharth is an undergraduate student in computer science and linguistics at UC Berkeley. </p> 
+    <h3> Siddharth Ganapathy </h3>
+        <p> Siddharth is an undergraduate student in computer science and linguistics at UC Berkeley. </p> 
 
-  <h3> Lindsay Hatch </h3>
-      <p> Lindsay is an undergraduate student in linguistics at UC Berkeley. </p> 
+    <h3> Lindsay Hatch </h3>
+        <p> Lindsay is an undergraduate student in linguistics at UC Berkeley. </p> 
 
-  <h3> Marie-Anne Xu </h3>
-      <p> Marie-Anne is an undergraduate student in EECS at UC Berkeley. </p> 
+    <h3> Marie-Anne Xu </h3>
+        <p> Marie-Anne is an undergraduate student in EECS at UC Berkeley. </p> 
 
       </div>
       }
@@ -132,39 +135,17 @@ var HomepageFR = React.createClass(
 
       <h1 className='ui dividing header'>Recueil de contes et chants Atchan </h1>
       <h2 >ÁCAN NANMƐ LÊ ÁLƐ́BHƆ́ </h2>
- 
-      {/* <h2> */}
-      {/* <div class="ui segment">
-      <div class="ui two column grid">
-        <div class="column">
-          <div class="ui segment">
-            <img className="ui floated medium spaced rounded image" src="./images/Anono_street.jpg"></img>
-          </div>
-        </div>
-        <div class="column">
-          <div class="ui segment">
-            <img className="ui floated medium spaced rounded image" src="./images/Blockhauss_ceremony.jpg"></img>
-          </div>
-        </div>
-      </div> */}
-      
-      {/* </h2> */}
-      <div class="ui segment">
-      <div class="ui medium images">
-        <img className="ui floated medium spaced rounded image" src="./images/Anono_street.jpg"></img>
-        <img className="ui floated medium spaced rounded image" src="./images/Blockhauss_ceremony.jpg"></img>
+
+      <div className="ui segment">
+      <img className="ui medium spaced rounded image" src="./images/Anono_street.jpg"></img>
+      <img className="ui medium spaced rounded image" src="./images/Blockhauss_ceremony.jpg"></img>
       </div>
-      {/* <div class="ui dividing header"></div> */}
-      </div>
-    {/* <div class="ui horizontal divider"></div> */}
-    {/* </div> */}
 
     <p> Ce site web fait la collection de textes, chansons et contes en langue atchan plus connue sous le nom ébrié. 
       Cette langue est parlée par les Ébrié qui se nomment eux-mêmes Tchaman et qui vivent dans une soixantaine de villages tous situés à Abidjan (actuelle capitale de la Côte d'Ivoire) et ses alentours. 
       Selon la classification de Williamson & Blench (2000:18), l'atchan forme, avec le nghlwa (mbatto), le sous-groupe Potou, lui-même issu du groupe Kwa appartenant à la Famille Niger-Congo. 
       Les éléments collectés ici vous permettront d'apprendre davantage sur le peuple, sa langue et ses us et coutumes.   </p> 
   
-    {/* </div>  */}
     <p> Ce site est une collaboration entre Dr Yao Maxime Dido, Professeur de linguistique à l'Université Alassane Ouattara de Bouaké (Côte d'Ivoire) et des linguistes de l'Université de Berkeley en Californie aux USA. 
       Dr Dido a participé à la mise en place de l'orthographe de l'atchan avec le Prof. Rémy Bôle-Richard, un linguiste spécialiste des langues Ouest-Africaines, qui en est le concepteur.</p>
     
